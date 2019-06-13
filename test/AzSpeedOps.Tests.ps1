009fea5d1c3367a4bb4f91e922778a86f334dae3
Windos <windosbk+github@gmail.com>
2019-06-14 09:04:14 +1200

    Forgot to update tests... that could have been bad

--- test/AzSpeedOps.Tests.ps1
+++ test/AzSpeedOps.Tests.ps1
@@ -1,28 +1,28 @@
-if (Get-Module -Name 'SpeedRun') {
-    Remove-Module -Name 'SpeedRun'
+if (Get-Module -Name 'AzSpeedOps') {
+    Remove-Module -Name 'AzSpeedOps'
 }
 
-if (Test-Path -Path "$PSScriptRoot/../SpeedRun") {
-    Import-Module "$PSScriptRoot/../SpeedRun/SpeedRun.psd1" -Force
+if (Test-Path -Path "$PSScriptRoot/../AzSpeedOps") {
+    Import-Module "$PSScriptRoot/../AzSpeedOps/AzSpeedOps.psd1" -Force
 } else {
-    Import-Module "$PSScriptRoot/../src/SpeedRun.psd1" -Force
+    Import-Module "$PSScriptRoot/../src/AzSpeedOps.psd1" -Force
 }
 
-Describe 'SpeedRun Module' {
+Describe 'AzSpeedOps Module' {
     Context 'Import Correctly' {
         It 'should be a script module' {
-            (Get-Module SpeedRun).ModuleType | Should -Be 'Script'
+            (Get-Module AzSpeedOps).ModuleType | Should -Be 'Script'
         }
 
         It 'should export two functions' {
-            (Get-Command -Module SpeedRun).Count | Should -Be 2
+            (Get-Command -Module AzSpeedOps).Count | Should -Be 2
         }
     }
 }
 
 Describe 'Get-NumberTrivia' {
     Context 'Standard Execution' {
-        Mock Invoke-RestMethod { '8 is the number of planets in the Solar System.' } -ModuleName SpeedRun -ParameterFilter {
+        Mock Invoke-RestMethod { '8 is the number of planets in the Solar System.' } -ModuleName AzSpeedOps -ParameterFilter {
             $Method -eq 'Get'
             $Uri -eq 'http://numbersapi.com/random/trivia'
         }
@@ -32,14 +32,14 @@ Describe 'Get-NumberTrivia' {
         }
 
         It 'called Invoke-RestMethod once' {
-            Assert-MockCalled Invoke-RestMethod -Times 1 -ModuleName SpeedRun
+            Assert-MockCalled Invoke-RestMethod -Times 1 -ModuleName AzSpeedOps
         }
     }
 }
 
 Describe 'Measure-RandomNumbers' {
     Context 'Standard Execution' {
-        Mock Get-RandomNumberOfNumbers { @(1, 2, 3, 4, 5) } -ModuleName SpeedRun
+        Mock Get-RandomNumberOfNumbers { @(1, 2, 3, 4, 5) } -ModuleName AzSpeedOps
 
         It 'returns expected sum' {
             Measure-RandomNumbers -Sum | Should -Be 15
@@ -51,7 +51,7 @@ Describe 'Measure-RandomNumbers' {
 
         It 'called Invoke-RestMethod once' {
             Measure-RandomNumbers
-            Assert-MockCalled Get-RandomNumberOfNumbers -Times 1 -ModuleName SpeedRun -Scope It
+            Assert-MockCalled Get-RandomNumberOfNumbers -Times 1 -ModuleName AzSpeedOps -Scope It
         }
     }
 }
